78c653f3970b9c11b9bdf04505a33760d49c7081
Enrique Hidalgo <koko.freelance@gmail.com>
2022-01-26 16:49:26 +0000

    bug->something with schema

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,16 +1,16 @@
 // import userModel from '../models/userModel.js';
 
-export const getUsers = (req, res) => {
-    res.json();
-}
+// export const getUsers = (req, res) => {
+//     res.json();
+// }
 
-export const createUsers = async (req, res) => {
-    try {
-        console.log(req.body);
-        // const newUser = new userModel({
-        // });
-        // await userModel.save(req.body);
-    } catch (error) {
-        console.log(error);
-    }
-}
+// export const createUsers = async (req, res) => {
+//     try {
+//         console.log(req.body);
+//         // const newUser = new userModel({
+//         // });
+//         // await userModel.save(req.body);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
