59a4b4dea5ed2f81827e3c26a244265702eec10a
tamtran <tamtran2885@gmail.com>
2022-01-28 23:14:11 +0100

    fix config

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -4,19 +4,19 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FB_APIKEY,
-  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FB_PROJECTID,
-  storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FB_APPID,
+  // apiKey: process.env.REACT_APP_FB_APIKEY,
+  // authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+  // projectId: process.env.REACT_APP_FB_PROJECTID,
+  // storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
+  // messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+  // appId: process.env.REACT_APP_FB_APPID,
 
-  // apiKey: "AIzaSyDCBdhjngokLzrz1YlOBxWDWruS2kZfwC8",
-  // authDomain: "apollofy-3e2c5.firebaseapp.com",
-  // projectId: "apollofy-3e2c5",
-  // storageBucket: "apollofy-3e2c5.appspot.com",
-  // messagingSenderId: "138714127658",
-  // appId: "1:138714127658:web:ad81b82e37ef12f36d5253",
+  apiKey: "AIzaSyDCBdhjngokLzrz1YlOBxWDWruS2kZfwC8",
+  authDomain: "apollofy-3e2c5.firebaseapp.com",
+  projectId: "apollofy-3e2c5",
+  storageBucket: "apollofy-3e2c5.appspot.com",
+  messagingSenderId: "138714127658",
+  appId: "1:138714127658:web:ad81b82e37ef12f36d5253",
 };
 
 // console.log(process.env.REACT_APP_FB_APIKEY)
