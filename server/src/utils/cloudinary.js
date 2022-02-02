f8d6ccc90e4d640ea3d84ba571b4828e1fa5a278
Enrique Hidalgo <koko.freelance@gmail.com>
2022-02-02 19:43:49 +0000

    fix-> dotenv backend

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,6 +1,6 @@
 import cloudinary from "cloudinary";
-// import dotenv from 'dotenv'
-// dotenv.config()
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 
@@ -8,13 +8,13 @@ import cloudinary from "cloudinary";
 
 
 cloudinary.v2.config({
-  cloud_name: "dj30eyyuy",
-  api_key: 842436786583383,
-  api_secret: "lSPIpVHqqaYRniS_Df0CrFbThb8"
+  // cloud_name: "dj30eyyuy",
+  // api_key: 842436786583383,
+  // api_secret: "lSPIpVHqqaYRniS_Df0CrFbThb8"
 
-  // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  // api_key: process.env.CLOUDINARY_API_KEY,
-  // api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // console.log(CLOUDINARY_CLOUD_NAME)
