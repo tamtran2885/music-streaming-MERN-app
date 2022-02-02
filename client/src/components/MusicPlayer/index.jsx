ac5035904f290ecc1727a5436a04246f3b812524
SalvaBandicoot <salvabandicoot@gmail.com>
2022-02-02 11:01:15 +0100

    Create Music Player Component

--- client/src/components/MusicPlayer/index.jsx
+++ client/src/components/MusicPlayer/index.jsx
@@ -5,7 +5,26 @@ const MusicPlayer = () => {
   return (
     <>
       <div className='musicplayer__absolute'>
-        Music Player
+        <div className='musicplayer__icon'>
+          <img src="Icon" alt="Icon" />
+        </div>
+        <div className='musicplayer__info'>
+          <div className='like'>Like</div>
+          <div className='musicplayer__info__song'>
+            <p className='tittle'>Song tittle</p>
+            <p className='artist'>Song Artist · Genre</p>
+          </div>
+          <div className='musicplayer__options'>Options</div>
+        </div>
+        <div className='musicplayer__player'>
+          <button>Repeat</button>
+          <button>Back</button>
+          <button>Play/Pause</button>
+          <button>Foward</button>
+          <button>Aleatory</button>
+        </div>
+        <div className='musicplayer__foward'></div>
+        <div className='musicplayer__volume'></div>
       </div>
     </>
   )
