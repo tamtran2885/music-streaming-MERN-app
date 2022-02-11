0b92e277ef0ebfacdf05bb68116ca636185d23c7
Enrique Hidalgo <koko.freelance@gmail.com>
2022-02-11 14:00:23 +0000

    albumsController in process

---
+++ server/src/controllers/albumsController.js
@@ -0,0 +1,7 @@
+
+
+export const getAlbums = async (req, res) => { }
+export const createAlbum = async (req, res) => { }
+export const getAlbumById = async (req, res) => { }
+export const updateAlbum = async (req, res) => { }
+export const deleteAlbum = async (req, res) => { }
