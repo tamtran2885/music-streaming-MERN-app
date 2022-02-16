188ee4a38d12fb7ae8cd80fc1b8107d1d3d6a931
Enrique Hidalgo <koko.freelance@gmail.com>
2022-02-16 12:40:45 +0000

    changes in serviceACcount

--- server/src/middlewares/firebaseConfig.js
+++ server/src/middlewares/firebaseConfig.js
@@ -7,7 +7,7 @@ import admin from "firebase-admin";
 admin.initializeApp({
   credential: admin.credential.cert(
     {
-      "projectId": process.env.FIREBASE_PROJECT_ID,
+      "project_id": process.env.FIREBASE_PROJECT_ID,
       "private_key": process.env.FIREBASE_PRIVATE_KEY,
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     }
