0c4bb5d0d4285db0e4376ded94f2c7d3b6e6ee98
Enrique Hidalgo <koko.freelance@gmail.com>
2022-02-16 12:34:08 +0000

    heroku

--- server/src/middlewares/firebaseConfig.js
+++ server/src/middlewares/firebaseConfig.js
@@ -1,11 +1,17 @@
 import admin from "firebase-admin";
 
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./serviceAccount.json");
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(
+    {
+      "projectId": process.env.FIREBASE_PROJECT_ID,
+      "private_key": process.env.FIREBASE_PRIVATE_KEY,
+      "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    }
+  ),
 });
 
 export default admin;
