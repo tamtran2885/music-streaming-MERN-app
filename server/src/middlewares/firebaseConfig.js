b584bbd48cb1fe3c19e004389759b47665918bcf
Enrique Hidalgo <koko.freelance@gmail.com>
2022-02-16 12:56:55 +0000

    wtf

--- server/src/middlewares/firebaseConfig.js
+++ server/src/middlewares/firebaseConfig.js
@@ -8,7 +8,7 @@ admin.initializeApp({
   credential: admin.credential.cert(
     {
       "project_id": process.env.FIREBASE_PROJECT_ID,
-      "private_key": process.env.FIREBASE_PRIVATE_KEY,
+      "private_key": process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     }
   ),
